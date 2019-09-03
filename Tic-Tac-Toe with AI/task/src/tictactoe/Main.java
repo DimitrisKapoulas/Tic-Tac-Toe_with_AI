75fc367ced640bf7b1c1690587132dde0cebf56b
DimitrisKapoulas <kapoulasd@gmail.com>
2019-09-03 21:00:24 +0300

    Create 2 methods fillArray and printGame

--- Tic-Tac-Toe with AI/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe with AI/task/src/tictactoe/Main.java
@@ -4,16 +4,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        char[] chars = fillArray();
+        printGame(chars);
+
+    }
+    private static char[] fillArray() {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
-        char[] chars = new char [9];
-        for(int i = 0; i < input.length() ; i++){
-            if(i >= 1 && i <10){
-                chars[i-1] = input.charAt(i);
+        char[] chars = new char[9];
+        for (int i = 0; i < input.length(); i++) {
+            if (i >= 1 && i < 10) {
+                chars[i - 1] = input.charAt(i);
             }
         }
-
-
+        return chars;
+    }
+    private static void printGame(char[] chars) {
         System.out.println("---------");
 
         System.out.println("| " + chars[0] + " " + chars[1] + " " + chars[2] + " |");
@@ -23,6 +30,6 @@ public class Main {
         System.out.println("| " + chars[6] + " " + chars[7] + " " + chars[8] + " |");
 
         System.out.println("---------");
-
     }
 }
+
